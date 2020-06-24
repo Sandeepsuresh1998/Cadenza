42f20b0619c6a9657654597c16d93ad7c35563e8
Sandeep Suresh <sureshsa@usc.edu>
2020-06-24 15:13:19 -0700

    More dependencies downloaded and getting the barebones of the backend going.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import styled, { keyframes } from 'styled-components';
 import {fadeInLeft} from 'react-animations';
-import {Button} from 'react-bootstrap-buttons';
-
+import axios from 'axios'
 
 const FadeInLeft = styled.div`animation: 2s ${keyframes`${fadeInLeft}`}`;
 
-function App() {
-  return (
-    <div className="App">
-      <FadeInLeft><h1 className="header">Music Taste</h1></FadeInLeft>
-      <Button className="loginButton">Login</Button>
-    </div>
-  );
+const axios = axios.create({
+  baseURL: "https://us-central1-musictaste-8ca96.cloudfunctions.net/api"
+})
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("Clicked");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FadeInLeft>
+          <h1 className="header">Music Taste</h1>
+          <button onClick={this.handleClick}className="loginButton">Login</button>
+        </FadeInLeft>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
