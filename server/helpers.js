479ae77b5524664e3cc7eb3a987ab55b6ca00d93
Sandeep Suresh <sureshsa@usc.edu>
2021-02-11 22:12:43 -0500

    Added the backend for top artists and removed a bug with duplicate entries.

--- server/helpers.js
+++ server/helpers.js
@@ -5,30 +5,27 @@ var client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
 var request = require('request'); // "Request" library
 
-function findMatchingTracks(myTracks, otherTracks) {
-    
-    //Should be same size
-    if(myTracks.length != otherTracks.length) {
-        console.log("Mismatch")
-    }
+function findMatchingItems(myItems, otherItems) {
 
     //TODO: Review algorithms to see if there is faster way to do this
     // O(n^2)
-    intersection = []
-    for (i in myTracks) {
-        for (k in otherTracks) {
-            if(myTracks[i].id == otherTracks[k].id) {
-                intersection.push(myTracks[i]);
+    idIntersection = new Set();
+    sharedItems = []
+    for (i in myItems) {
+        for (k in otherItems) {
+            if(myItems[i].id == otherItems[k].id && !idIntersection.has(myItems[i].id)) {
+                sharedItems.push(myItems[i]);
+                idIntersection.add(myItems[i].id);
             }
         }
     }
 
-    //Intersectoin of top songs
-    for (i in intersection) {
-        console.log(intersection[i].name)
+    //Intersection of top songs
+    for (i in sharedItems) {
+        console.log(sharedItems[i].name);
     }
     
-    return intersection;
+    return sharedItems;
 }
 
 async function getNewAccessToken(refreshToken) {
@@ -51,8 +48,13 @@ async function getNewAccessToken(refreshToken) {
     return token;
 }
 
-function compareTwoSongs(track1, track2) {
-    return track1.id == track2.id;
+function getArtistsHelper(timeRange, accessToken) {
+    //Create header
+    const config = {
+        headers: {Authorization : `Bearer ${accessToken}`},
+    }
+
+    return axios.get("https://api.spotify.com/v1/me/top/artists?time_range=" + timeRange, config).then(response => response.data.items)
 }
 
 function getTracksHelper(timeRange, accessToken) {
@@ -67,9 +69,6 @@ function getTracksHelper(timeRange, accessToken) {
 async function getSharedTracks(myAccessToken, otherAccessToken) {
     //All the requests for all the different ranges we get access to
 
-    console.log("Sending this access: " + myAccessToken);
-    console.log("Sending this otherAccessToken: " + otherAccessToken);
-
     const similarTracks = await axios.all([
         getTracksHelper('short_term', myAccessToken),
         getTracksHelper('medium_term', myAccessToken),
@@ -78,14 +77,36 @@ async function getSharedTracks(myAccessToken, otherAccessToken) {
         getTracksHelper('medium_term', otherAccessToken),
         getTracksHelper('long_term', otherAccessToken)
     ]).then(axios.spread((...responses) => {
+        console.log()
         myTracks = [...responses[0], ...responses[1],...responses[2]]
         otherTracks = [...responses[3], ...responses[4],...responses[5]]
-        return findMatchingTracks(myTracks, otherTracks)
+        return findMatchingItems(myTracks, otherTracks)
     })).catch(error => {
         console.log(error);
     });
     return similarTracks;
 }
 
+async function getSharedArtists(myAccessToken, otherAccessToken) {
+    const similarArtists = await axios.all([
+        getArtistsHelper('short_term', myAccessToken),
+        getArtistsHelper('medium_term', myAccessToken),
+        getArtistsHelper('long_term', myAccessToken),
+        getArtistsHelper('short_term', otherAccessToken),
+        getArtistsHelper('medium_term', otherAccessToken),
+        getArtistsHelper('long_term', otherAccessToken)
+    ]).then(axios.spread((...responses) => {
+        console.log(responses[0]);
+        myTracks = [...responses[0], ...responses[1],...responses[2]]
+        otherTracks = [...responses[3], ...responses[4],...responses[5]]
+        return findMatchingItems(myTracks, otherTracks)
+        
+    })).catch(error => {
+        console.log(error);
+    });
 
-module.exports = {getNewAccessToken, getSharedTracks}
+    return similarArtists;
+}
+
+
+module.exports = {getNewAccessToken, getSharedTracks, getSharedArtists}
--- server/server.js
+++ server/server.js
@@ -396,13 +396,53 @@ app.get('/getSharedTopTracks', (req, res, next) => {
     // Couldn't find me in the db
     res.send(err).status(501);
   }) 
-
-  
-
-  
-  console.log("End");
-
 });
+
+//FUNCTION: Return matches in two users' top artists
+app.get('/getSharedTopArtists', (req, res, next) => {
+
+  //In the parameters we have two user ids
+  //Return top artists in short, medium, long term
+  
+  //For now by "my" I mean the logged in user
+  const myUserId = req.query.me;
+  const otherUserId = req.query.other;
+  var otherAccessToken;
+  var myAccessToken;
+
+  //Hit Db for acess_tokens from each
+  //Get user's access and refresh
+  db.collection("Users").doc(myUserId).get().then(doc => {
+
+    console.log("Starting the get shared top reqs");
+    if(!doc.exists) {
+      res.send("Unable to find logged in user").status(500);
+    }
+    helper.getNewAccessToken(doc.data().refresh_token).then(token => {
+      myAccessToken = token;
+    })
+
+    //Get other's new access token
+    db.collection("Users").doc(otherUserId).get().then(doc => {
+      if(!doc.exists) {
+        res.send("Unable to find logged in user").status(500);
+      }
+      helper.getNewAccessToken(doc.data().refresh_token).then(token => {
+        otherAccessToken = token;
+        //Call helper to get all the same shared top tracks
+        helper.getSharedArtists(myAccessToken, otherAccessToken);
+      })
+    }).catch(err => {
+      //Couldn't find other in the db
+      res.send(err).status(501);
+    });
+    
+  }).catch(err => {
+    // Couldn't find me in the db
+    res.send(err).status(501);
+  }) 
+});
+
 
 console.log("Listening on 8888")
 app.listen(process.env.PORT || 8888);
